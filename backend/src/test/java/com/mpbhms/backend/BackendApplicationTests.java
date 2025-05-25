fbf71a416a2b7c6cb50842942e8bfa03ca060a49
nguyenphuong <yugioher112@gmail.com>
2025-05-25 23:29:25 +0700

    Add spring boot project to backend folder

---
+++ backend/src/test/java/com/mpbhms/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpbhms.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
