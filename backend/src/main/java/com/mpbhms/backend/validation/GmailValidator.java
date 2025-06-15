2479c2eac3b4257db17a02b3f14fc8d7c8592f79
nguyenphuong <yugioher112@gmail.com>
2025-06-15 09:08:32 +0700

    validate gmail

--- backend/src/main/java/com/mpbhms/backend/validation/GmailValidator.java
+++ backend/src/main/java/com/mpbhms/backend/validation/GmailValidator.java
@@ -5,13 +5,19 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class GmailValidator implements ConstraintValidator<Gmail, String> {
 
-    private static final String GMAIL_REGEX = "^[A-Za-z0-9._%+-]+@gmail\\.com$";
+    private static final String VIETNAM_EMAIL_REGEX =
+            "^[A-Za-z0-9._%+-]+@(gmail\\.com(\\.vn)?"
+                    + "|fpt\\.edu\\.vn"
+                    + "|student\\.hust\\.edu\\.vn"
+                    + "|hcmut\\.edu\\.vn"
+                    + "|stu\\.edu\\.vn"
+                    + "|vnuit\\.edu\\.vn"
+                    + "|[A-Za-z0-9.-]+\\.edu\\.vn"
+                    + ")$";
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext context) {
-        if (email == null) {
-            return false;
-        }
-        return email.matches(GMAIL_REGEX);
+        if (email == null) return false;
+        return email.matches(VIETNAM_EMAIL_REGEX);
     }
 }
