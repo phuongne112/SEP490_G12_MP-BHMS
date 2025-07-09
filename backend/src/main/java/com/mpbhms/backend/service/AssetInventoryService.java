e7bee931a0489360ed7c19e239b393401cc8dfe2
nguyenphuong <yugioher112@gmail.com>
2025-07-09 14:26:45 +0700

    checkin-checkout asset

--- backend/src/main/java/com/mpbhms/backend/service/AssetInventoryService.java
+++
@@ -1,8 +0,0 @@
-package com.mpbhms.backend.service;
-
-import com.mpbhms.backend.dto.AssetInventoryRequest;
-import java.util.List;
-
-public interface AssetInventoryService {
-    void saveCheckinAssets(List<AssetInventoryRequest> requestList);
-} 
