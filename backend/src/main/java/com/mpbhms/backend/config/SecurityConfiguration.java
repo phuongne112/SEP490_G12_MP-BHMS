e847c3090acfb97c9404e800685e2a3088b7842e
nguyenphuong <yugioher112@gmail.com>
2025-08-17 10:21:37 +0700

    log

--- backend/src/main/java/com/mpbhms/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/mpbhms/backend/config/SecurityConfiguration.java
@@ -90,7 +90,8 @@ public class SecurityConfiguration {
                 "http://localhost:8080",
                 "http://localhost:5173",
                 "http://localhost",
-                "http://20.78.57.200"
+                "http://20.78.57.200",
+                "http://52.184.69.15"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "x-no-retry"));
